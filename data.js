db3b138f6eda237183422042e687b0ea38bc15ba
Hoàng Văn Việt Anh <hoangvanvietanh@gmail.com>
2020-09-11 18:49:22 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,59 @@
+var data = [{
+    "topic": "Số ít",
+    "words":[
+        {
+            "word":"你",
+            "spelling":"nǐ",
+            "meaning":"bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3.mp3",
+            "writing":"...."
+        },{
+            "word":"你",
+            "spelling":"nǐ",
+            "meaning":"bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3.mp3",
+            "writing":"...."
+        },{
+            "word":"你",
+            "spelling":"nǐ",
+            "meaning":"bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3.mp3",
+            "writing":"...."
+        },{
+            "word":"你",
+            "spelling":"nǐ",
+            "meaning":"bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3.mp3",
+            "writing":"...."
+        }
+    ]
+},{
+    "topic": "Số nhiều",
+    "words":[
+        {
+            "word":"你们",
+            "spelling":"nǐmen",
+            "meaning":"các bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3men5.mp3",
+            "writing":"...."
+        },{
+            "word":"你们",
+            "spelling":"nǐmen",
+            "meaning":"các bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3men5.mp3",
+            "writing":"...."
+        },{
+            "word":"你们",
+            "spelling":"nǐmen",
+            "meaning":"các bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3men5.mp3",
+            "writing":"...."
+        },{
+            "word":"你们",
+            "spelling":"nǐmen",
+            "meaning":"các bạn",
+            "sound":"https://toihoctiengtrung.com/wp-content/uploads/audio/course/ni3men5.mp3",
+            "writing":"...."
+        }
+    ]
+}]
